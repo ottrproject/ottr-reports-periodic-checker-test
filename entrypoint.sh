1afa2e00da9e289a5c3073a015ba8745e64a4eab
Candace Savonen <cansav09@gmail.com>
2022-03-22 12:07:19 -0400

    Add things

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,29 @@
 #!/bin/sh -l
 
-echo "Hello $1"
-time=$(date)
-echo "::set-output name=time::$time"
+set -e
+
+printf "Found files in workspace:\n"
+ls
+
+printf "Looking for which report to be done...\n"
+
+if ${{inputs.check_type == 'spelling'}} ;then
+  error_name='Spelling errors'
+  report_path='resources/spell_check_results.tsv'
+  chk_results="Rscript scripts/spell-check.R"
+elif ${{inputs.check_type == 'urls'}} ;then
+  error_name='Broken URLs'
+  report_path='resources/url_checks.tsv'
+  chk_results="Rscript scripts/url-check.R"
+elif ${{inputs.check_type ==  'quiz_format'}} ;then
+  error_name='Quiz format errors'
+  report_path='question_error_report.tsv'
+  chk_results="Rscript -e 'ottrpal::check_quizzes(quiz_dir = 'quizzes', write_report = TRUE, verbose = TRUE)'"
+fi
+
+printf $error_name
+printf $report_path
+${chk_results}
+
+chk_results="$((chk_results-1))"
+echo $chk_results
