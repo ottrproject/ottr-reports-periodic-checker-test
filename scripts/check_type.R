1d17d9bd8730a38476d670e6a0d2ba56db4430e7
Candace Savonen <cansav09@gmail.com>
2022-03-29 14:16:29 -0400

    change files paths in check_type.R

--- scripts/check_type.R
+++ scripts/check_type.R
@@ -7,6 +7,6 @@ check_type <- readLines('check_type.txt')
 
 print(paste("Check type specified:", check_type))
 
-if (check_type == "spelling") source("scripts/spell-check.R")
-if (check_type == "urls") source("scripts/url-check.R")
-if (check_type == "quiz_format") source("scripts/quiz-check.R")
+if (check_type == "spelling") source("ottr_report_scripts/spell-check.R")
+if (check_type == "urls") source("ottr_report_scripts/url-check.R")
+if (check_type == "quiz_format") source("ottr_report_scripts/quiz-check.R")
