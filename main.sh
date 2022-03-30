778e2ff206ea4406fddd61f3156ddab56d4df218
Candace Savonen <cansav09@gmail.com>
2022-03-30 09:06:31 -0400

    Update main.sh to make it clearer

--- main.sh
+++ main.sh
@@ -14,31 +14,30 @@ script_directory="$(perl -e 'use File::Basename;
 printf "running from: $script_directory"
 echo $INPUT_CHECK_TYPE >> check_type.txt
 
-# Copy the scripts from this version
-curl -o $script_directory/spell-check.R https://raw.githubusercontent.com/jhudsl/ottr-reports/main/scripts/spell-check.R
-curl -o $script_directory/url-check.R https://raw.githubusercontent.com/jhudsl/ottr-reports/main/scripts/url-check.R
-curl -o $script_directory/quiz-check.R https://raw.githubusercontent.com/jhudsl/ottr-reports/main/scripts/quiz-check.R
-
 if [ "${INPUT_CHECK_TYPE}" == "spelling" ];then
+  curl -o $script_directory/spell-check.R https://raw.githubusercontent.com/jhudsl/ottr-reports/main/scripts/spell-check.R
   error_name='Spelling errors'
   report_path='resources/spell_check_results.tsv'
   chk_results=$(Rscript $script_directory/spell-check.R)
+  rm $script_directory/spell-check.R
 elif [ "${INPUT_CHECK_TYPE}" == "urls" ];then
+  curl -o $script_directory/url-check.R https://raw.githubusercontent.com/jhudsl/ottr-reports/main/scripts/url-check.R
   error_name='Broken URLs'
   report_path='resources/url_checks.tsv'
   chk_results=$(Rscript $script_directory/url-check.R)
+  rm $script_directory/url-check.R
 elif [ "${INPUT_CHECK_TYPE}" == "quiz_format" ];then
+  curl -o $script_directory/quiz-check.R https://raw.githubusercontent.com/jhudsl/ottr-reports/main/scripts/quiz-check.R
   error_name='Quiz format errors'
   report_path='question_error_report.tsv'
   chk_results=$(Rscript $script_directory/quiz-check.R)
+  rm $script_directory/quiz-check.R
 fi
 
 # Print out the output
 printf $error_name
 printf $report_path
 printf $chk_results
-
-rm -rf $script_directory
 
 # Save output
 echo ::set-output name=error_name::$error_name
