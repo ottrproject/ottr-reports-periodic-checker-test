5bd99dccf3fcca6837e7fad931af137553a62c28
Candace Savonen <cansav09@gmail.com>
2022-08-18 09:34:20 -0400

    Turn back to quote

--- main.sh
+++ main.sh
@@ -17,19 +17,19 @@ echo $INPUT_CHECK_TYPE >> check_type.txt
 
 if [ "${INPUT_CHECK_TYPE}" == "spelling" ];then
   curl -o $script_directory/spell-check.R https://raw.githubusercontent.com/jhudsl/ottr-reports/main/scripts/spell-check.R
-  error_name=$(echo Spelling errors)
+  error_name='Spelling errors'
   report_path=check_reports/spell_check_results.tsv
   chk_results=$(Rscript $script_directory/spell-check.R)
   rm $script_directory/spell-check.R
 elif [ "${INPUT_CHECK_TYPE}" == "urls" ];then
   curl -o $script_directory/url-check.R https://raw.githubusercontent.com/jhudsl/ottr-reports/main/scripts/url-check.R
-  error_name=$(echo Broken URLs)
+  error_name='Broken URLs'
   report_path=check_reports/url_checks.tsv
   chk_results=$(Rscript $script_directory/url-check.R)
   rm $script_directory/url-check.R
 elif [ "${INPUT_CHECK_TYPE}" == "quiz_format" ];then
   curl -o $script_directory/quiz-check.R https://raw.githubusercontent.com/jhudsl/ottr-reports/main/scripts/quiz-check.R
-  error_name=$(echo Quiz format errors)
+  error_name='Quiz format errors'
   report_path=check_reports/question_error_report.tsv
   chk_results=$(Rscript $script_directory/quiz-check.R)
   rm $script_directory/quiz-check.R
