c165c245a3096b219ae6badb99911bb7fb730207
Candace Savonen <cansav09@gmail.com>
2022-08-18 08:46:45 -0400

    Update main.sh

--- main.sh
+++ main.sh
@@ -3,14 +3,6 @@
 set -e
 set -o pipefail
 
-# This script should always run as if it were being called from
-# the directory it lives in.
-script_directory="$(perl -e 'use File::Basename;
-  use Cwd "abs_path";
-  print dirname(abs_path(@ARGV[0]));' -- "$0")"
-
-printf "Running from: $script_directory \n"
-
 echo $INPUT_CHECK_TYPE >> check_type.txt
 
 if [ "${INPUT_CHECK_TYPE}" == "spelling" ];then
